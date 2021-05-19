002d7220b0f0a31c4e1e43ae71a7201b403237ee
Michael Newman <therealmichaelnewman@gmail.com>
2021-05-19 14:53:26 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -31,10 +31,9 @@ function ternaryTeenager(age) {
 function switchAge(age) {
   switch (age) {
     case (age >= 13 && age <= 19):
-      console.log("You are a teenager");
-      break;
+      return "You are a teenager")
       default:
-      console.log("You have an age");
+      return "You have an age"
     }
 
 }
